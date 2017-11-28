4abb37ac4931e0669763e2d2b4e96246fd5335ba
DeadFred <f.cliff1@hotmail.com>
2017-11-28 14:09:02 +1000

    Tinder profile app in React

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Profile extends Component {
-  render() {
-    return (
-        <div className="profile">
-          <div>
-            <img src={ this.props.imageURL } alt="User" />
-          </div>
-          <p>Name: { this.props.firstName } { this.props.lastName }</p>
-        </div>
-    );
-  }
-}
+const Profile = ({ firstName, lastName, imageURL }) => {
+  return (
+    <div className="profile">
+      <div>
+        <img src={imageURL} alt="User" />
+      </div>
+      <p>Name: {firstName} {lastName}</p>
+    </div>
+  );
+};
 
 export default Profile;
